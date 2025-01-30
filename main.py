67028ffc5623a83fbc99ebab5881fab33d71092c
shrestak <77411643+shrestak@users.noreply.github.com>
2025-01-30 19:29:15 +0000

    end of class

--- main.py
+++ main.py
@@ -19,3 +19,32 @@ multilinestring = """
 """
 
 print(h+str(n1))
+
+# Formatted Strings
+
+n1 = 5
+n2 = 3
+out = f"{n1}+{n2}={n1+n2}"
+print(out)
+
+# Logical operators
+# Code block
+
+n1=5
+n2=5
+if n1<n2:
+    print("Lesser")
+    print("<"*6)
+elif n1==n2:
+    print("Equal")
+    print('====')
+else:
+    print("Greater")
+    print(">"*len("Greater"))
+
+
+# blocks in python use indentation
+# used in if statements/functions def
+
+f = 17.4
+print(type(f))
