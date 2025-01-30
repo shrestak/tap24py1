eb01998c9d41201fbefaca9148223cb0ea392c91
shrestak <77411643+shrestak@users.noreply.github.com>
2025-01-30 19:06:45 +0000

    begin

---
+++ main.py
@@ -0,0 +1,21 @@
+# Basic data types
+
+h = "hello"
+w = "world"
+
+print(h,w)
+print(h+w)
+
+n1 = 5
+# print(h+n1)
+
+# How to concatenate str with number?
+# typecasting
+
+multilinestring = """
+    str() - cast to string
+    int() - cast to integer
+    float() - cast to float
+"""
+
+print(h+str(n1))
